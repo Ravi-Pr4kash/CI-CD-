35fcc815a32a30457aced991c1fc21d25f6e424a
Ravi-Pr4kash <raviprakash0818@gmail.com>
2025-05-13 21:29:45 +0530

    Initial commit with CI setup 2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import express from 'express';
-
+// index.js
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-    res.send("Hello World")
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
